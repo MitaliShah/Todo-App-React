fa098d397661ed35f98f5537deaf5471f8ff6793
Mitali Shah <mitalishaleenshah@gmail.com>
2019-03-09 18:57:57 -0800

    changed component to class based component and added state

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React from 'react'
 import ToDoItem from './components/ToDoItem'
 import todosData from './todosData'
 
-function App(){
-    const todoItems = todosData.map(item => <ToDoItem key={item.id}item={item}/>)
+class App extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            todos: todosData            
+        }        
+    }
+    render(){
+        const todoItems = this.state.todosData.map(item => <ToDoItem key={item.id}item={item}/>)
     return(
         <div className="todo-list">
            {todoItems}
         </div>
     )
+    }
+    
 }
 
 export default App
