120658a1590c5d5f776b64fba93a80a82e028065
Mitali Shah <mitalishaleenshah@gmail.com>
2019-04-20 13:56:29 -0700

    added state to app component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import React from 'react'
 import ToDoItem from './components/ToDoItem'
 import todosData from './todosData'
 
-function App(){
-    const todoItems = todosData.map(item => <ToDoItem key={item.id}item={item}/>)
-    return(
-        <div className="todo-list">
-           {todoItems}
-        </div>
-    )
+/*
+In the previous iteration of this todo list app, we pulled in todos data from a JSON file and mapped over it to display the todo items.
+
+Eventually we'll want to be able to modify the data, which will only happen if we've "loaded" the data in to the component's state
+
+Challenge: Change the <App /> component into a stateful class component and load the imported `todosData` into state.
+*/
+
+class App extends React.Component {
+
+    constructor(){
+        super()
+        this.state = {
+            todos: todosData
+        }
+    }   
+
+    render(){
+        const todoItems = this.state.todos.map(item => <ToDoItem key={item.id}item={item}/>)
+            return(
+            <div className="todo-list">
+            {todoItems}
+            </div>
+        )
+    }
+    
 }
 
 export default App
