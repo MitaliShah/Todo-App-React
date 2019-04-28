e582f26d927cde05ff90544500e46013fd7d9ae8
Mitali Shah <mitalishaleenshah@gmail.com>
2019-04-27 18:34:06 -0700

    added conditional rendering when the list is checked apply new style

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,10 +1,21 @@
 import React from 'react'
 
+/**
+ * Challenge: Style the completed todo items differently from the incomplete items.
+ */
+
 function ToDoItem(props){
+
+    const completedStyle = {
+        fontStyle: "italic",
+        color: "#cdcdcd",
+        textDecoration: "line-through"
+    }
+
     return(
     <div className="todo-item">
         <input type="checkbox" checked={props.item.completed} onChange={()=> props.handleChange(props.item.id)}/>
-        <p>{props.item.text}</p>
+        <p style={props.item.completed ? completedStyle : null}>{props.item.text}</p>
     </div>
     )
 
