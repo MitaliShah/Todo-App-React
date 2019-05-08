f5cdfe85204301b9831205fd77095bdd661c3774
Mitali Shah <mitalishaleenshah@gmail.com>
2019-05-07 18:30:09 -0700

    Updates

---
+++ precache-manifest.4e5f266e0fea10f7039fbcbb9453aca6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9aab96a8d40c5eb8e36b",
+    "url": "/Todo-App-React/static/js/runtime~main.9aab96a8.js"
+  },
+  {
+    "revision": "6584d36192643619c542",
+    "url": "/Todo-App-React/static/js/main.6584d361.chunk.js"
+  },
+  {
+    "revision": "015ea03cb7b11a05a1c5",
+    "url": "/Todo-App-React/static/js/2.015ea03c.chunk.js"
+  },
+  {
+    "revision": "6584d36192643619c542",
+    "url": "/Todo-App-React/static/css/main.6fb7cdd6.chunk.css"
+  },
+  {
+    "revision": "b5acb0293288b5bedef602b08c53ff5e",
+    "url": "/Todo-App-React/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MitaliShah/Todo-App-React/precache-manifest.5e5da4a8df443b1aec723c376ecfe67f.js"
+  "/Todo-App-React/precache-manifest.4e5f266e0fea10f7039fbcbb9453aca6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/MitaliShah/Todo-App-React/index.html", {
+workbox.routing.registerNavigationRoute("/Todo-App-React/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
